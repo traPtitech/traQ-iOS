ea78a48b6c22ae01c056d76af8f7ec8db458cf64
とーふとふ <tohu.soy@gmail.com>
2018-06-20 23:36:25 +0900

    Initial Commit

---
+++ traQ-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  traQ-iOS
+//
+//  Created by とーふとふ on 2018/06/20.
+//  Copyright © 2018年 traP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
